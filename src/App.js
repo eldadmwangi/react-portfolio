176d8e79f36e61061d322c0382fece0b778a1109
eldadmwangi <eldadmwangi@gmail.com>
2021-04-04 05:07:49 +0300

    added footer styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Projects from './components/Projects';
 import AboutUs from './components/AboutUs';
 import Home from './components/Home';
 import Contacts from './components/Contacts';
+// import Particles from 'react-particles-js';
 
 function App() {
   return (
@@ -14,8 +15,28 @@ function App() {
     
     <BrowserRouter>
      <Navbar/>
+     {/* <Particles 
+       params={{
+         particles:{
+           number:{
+             value:30,
+             density:{
+               enable:true,
+               value_area:900
+             }
+           },
+           shape:{
+             type:'star',
+             stroke:{
+                width:6,
+                color:'#000000'
+             }
+           }
+         }
+       }}/> */}
      <Switch>
        <Route path='/' exact component={Home}/>
+      
        <Route path='/AboutUs' exact component={AboutUs}/>
        <Route path='/Services' exact component={Services}/>
        <Route path='/Projects' exact component={Projects}/>
